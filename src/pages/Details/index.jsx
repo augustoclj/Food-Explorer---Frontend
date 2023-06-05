a4593c2c756c2104fa1abc432197694ee364ce91
Augusto Julietti <augusto.julietti@gmail.com>
2023-06-05 16:02:02 -0300

    Criando CSS inJS, Theme e Global Style

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles.js"
+
+export function Details(){
+
+  return(
+    <Container>
+      <h1>Food Explorer</h1>
+      <span>Desafio Final Explorer</span>
+    </Container>
+
+  );
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+export default{
+  COLORS: {
+    BACKGROUND_900: "#232129",
+    BACKGROUND_800: "#312E38",
+    RED: "#FF002E",
+    WHITE: "#FFF"
+  }
+}
